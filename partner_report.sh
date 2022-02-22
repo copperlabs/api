97e010f93d571df66347312571e7d3bab9eec1f0
Matt Greene <matt@copperlabs.com>
2022-02-22 15:47:42 -0700

    Add meter type breakout to partner report

--- partner_report.sh
+++ partner_report.sh
@@ -19,14 +19,23 @@ health_report=${reports_dir}/health_history.${handle}.${report_date}.csv
 echo "Compiling health history for ${handle}"
 python copper-enterprise-client.py --csv-output-file ${health_report} report health
 
-echo "prems created:                ${num_prems}"
-echo "mobile users:                 `cat ${prem_report} | grep -c has_mobile_app`"
-echo "total gateways:               `cat ${health_report} | grep -c gateway`"
-echo "active gateways:              `cat ${health_report} | grep gateway | grep -c active`"
-echo "disconnected gateways:        `cat ${health_report} | grep gateway | grep -vc active`"
-echo "total meters:                 `cat ${health_report} | grep -c meter`"
-echo "connected meters:             `cat ${health_report} | grep meter | grep -c ,connected`"
-echo "degraded/disconnected meters: `cat ${health_report} | grep meter | grep -vc ,connected`"
+echo "prems created:                         ${num_prems}"
+echo "mobile users:                          `cat ${prem_report} | grep -c has_mobile_app`"
+echo "total gateways:                        `cat ${health_report} | grep -c gateway`"
+echo "active gateways:                       `cat ${health_report} | grep gateway | grep -c active`"
+echo "disconnected gateways:                 `cat ${health_report} | grep gateway | grep -vc active`"
+echo "total meters:                          `cat ${health_report} | grep -c meter`"
+echo "connected meters:                      `cat ${health_report} | grep meter | grep -c ,connected`"
+echo "degraded/disconnected meters:          `cat ${health_report} | grep meter | grep -vc ,connected`"
+echo "total electric meters:                 `cat ${health_report} | grep -c ,power`"
+echo "connected electric meters:             `cat ${health_report} | grep ,power | grep -c ,connected`"
+echo "degraded/disconnected electric meters: `cat ${health_report} | grep ,power | grep -vc ,connected`"
+echo "total gas meters:                      `cat ${health_report} | grep -c ,gas`"
+echo "connected gas meters:                  `cat ${health_report} | grep ,gas | grep -c ,connected`"
+echo "degraded/disconnected gas meters:      `cat ${health_report} | grep ,gas | grep -vc ,connected`"
+echo "total water meters:                    `cat ${health_report} | grep -c ,water`"
+echo "connected water meters:                `cat ${health_report} | grep ,water | grep -c ,connected`"
+echo "degraded/disconnected water meters:    `cat ${health_report} | grep ,water | grep -vc ,connected`"
 
 cd ${reports_dir}
 report=${handle}.reports.${report_date}.zip
